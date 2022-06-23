01b8baea50a6a964da5490d2ad81ce5123ba7edb
ArratiaLab2 <arratialab@gmail.com>
2022-06-23 15:59:07 -0700

    June 22 data and analysis code update

--- run_scan1.sh
+++ run_scan1.sh
@@ -1,27 +1,27 @@
-(sleep 2; echo "hi") | python StepperMotor/move.py -100,0
+(sleep 2; echo "hi") | python StepperMotor/move.py -65,0
 rcdaq_client load librcdaqplugin_drs.so
 sleep 2
-rcdaq_client create_device device_drs -- 1 1001 0x22 100 positive 550 0 0 1024 0
+rcdaq_client create_device device_drs -- 1 1001 0x28 400 positive 100 3
 sleep 2
 rcdaq_control.pl &
 sleep 2
 rcdaq_client daq_open
 
-for i in {0..3000..100}
+for i in {0..2600..65}
 	do
-		for j in {0..3000..100}
+		for j in {0..2600..65}
 			do
 				echo "moving to $i,$j"
-				(sleep 2; echo "hi") | python StepperMotor/move.py 100,0
+				(sleep 2; echo "hi") | python StepperMotor/move.py 65,0
 				sleep 3
 				rcdaq_client daq_begin
 				sleep 2
 				rcdaq_client daq_end
 			done
 		
-		(sleep 2; echo "hi") | python StepperMotor/move.py -3000,100
+		(sleep 2; echo "hi") | python StepperMotor/move.py -2600,65
                 sleep 10
-		(sleep 2; echo "hi") | python StepperMotor/move.py -100,0
+		(sleep 2; echo "hi") | python StepperMotor/move.py -65,0
 		sleep 3
 
 	done 
